d258957057b56ae0765715c65305c0a836283acc
JohnCS <myers.1091@gmail.com>
2019-11-27 12:31:04 -0800

    added option to use trained network

--- resnet50_classifier.py
+++ resnet50_classifier.py
@@ -205,29 +205,32 @@ else:
 
 #Create resnet50 model
 #
+#To train from scatch
+# model = ResNet50(input_shape = input_shape, classes = 184)
+# model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+#To train from previous weights
+model = load_model('weights/weights-05-0.0621_1.h5')
 
-model = ResNet50(input_shape = input_shape, classes = 184)
-model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 
 train_datagen = ImageDataGenerator(rescale = 1./255,
 shear_range = 0.2,
 zoom_range = 0.2,
 horizontal_flip = True)
 test_datagen = ImageDataGenerator(rescale = 1./255)
-training_set = train_datagen.flow_from_directory('/Users/john/Documents/DataSci/Leaves/colorleaves/train/',
+training_set = train_datagen.flow_from_directory('/home/johncs/Documents/Plant_Classifier/colorleaves/train/',
 target_size = (64, 64),
 batch_size = 32,
 class_mode = 'categorical')
-test_set = test_datagen.flow_from_directory('/Users/john/Documents/DataSci/Leaves/colorleaves/val/',
+test_set = test_datagen.flow_from_directory('/home/johncs/Documents/Plant_Classifier/colorleaves/val/',
 target_size = (64, 64),
 batch_size = 32,
 class_mode = 'categorical')
 
 log_dir="logs/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
-tensorboard_callback = TensorBoard(log_dir=log_dir, histogram_freq=1)
-early_stopping_monitor = EarlyStopping(patience=2)
+tensorboard_callback = TensorBoard(log_dir=log_dir, histogram_freq=0,write_graph=True, write_images=True)
+early_stopping_monitor = EarlyStopping(monitor='val_loss', mode='min',patience=2)
 #define the checkpoint
-filepath = '/Users/john/Documents/DataSci/Leaves/weights/weights-{epoch:02d}-{loss:.4f}.h5'
+filepath = '/home/johncs/Documents/Plant_Classifier/weights/weights-{epoch:02d}-{loss:.4f}_2.h5'
 checkpoint = ModelCheckpoint(filepath, monitor = 'loss', verbose = 1, save_best_only = True, mode = 'min')
 
 model.fit_generator(training_set,steps_per_epoch = 6109,
@@ -238,11 +241,11 @@ callbacks=[tensorboard_callback,early_stopping_monitor,checkpoint])
 
 
 
-import numpy as np
-from keras.preprocessing import image
-test_image = image.load_img('/Users/john/Documents/DataSci/Leaves/P7293143_gr.jpg', target_size = (64, 64))
-test_image = image.img_to_array(test_image)
-test_image = np.expand_dims(test_image, axis = 0)
-result = classifier.predict(test_image)
-training_set.class_indices
-print(result)
+# import numpy as np
+# from keras.preprocessing import image
+# test_image = image.load_img('/Users/john/Documents/DataSci/Leaves/P7293143_gr.jpg', target_size = (64, 64))
+# test_image = image.img_to_array(test_image)
+# test_image = np.expand_dims(test_image, axis = 0)
+# result = classifier.predict(test_image)
+# training_set.class_indices
+# print(result)
