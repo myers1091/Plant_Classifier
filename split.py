3a7567ea62b099fc4a0e495b3fe180f2160f8cfd
John Myers <john@Johns-MacBook-Pro.local>
2019-11-27 17:28:41 -0800

    removed hard coding

--- split.py
+++ split.py
@@ -1,3 +1,14 @@
 import split_folders
+import argparse
 
-split_folders.ratio('/Users/john/Documents/DataSci/Leaves/RGB',output = '/Users/john/Documents/DataSci/Leaves/output', seed = 1337, ratio=(0.8,0,0.2))
+parser = argparse.ArgumentParser(description='Basic Arg parser')
+parser.add_argument("--input", default=1, type=str, help="Input Directory",required = True)
+parser.add_argument("--output", default=1, type=str, help="Output Directory",required = True)
+parser.add_argument("--ratio", nargs='+',help="Split ratio, default 0.8,0.1,0.1",required = True)
+
+args = parser.parse_args()
+indir = args.input
+outdir = args.output
+ratio = (float(args.ratio[0]),float(args.ratio[1]),float(args.ratio[2]))
+
+split_folders.ratio(indir,output = outdir, seed = 1337, ratio = ratio)
