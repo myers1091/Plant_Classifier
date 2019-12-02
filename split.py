c26efd85d7a4fa925a8584f2a4bad491ca203801
John Myers <john@Johns-MacBook-Pro.local>
2019-12-02 12:07:14 -0800

    fixed ratio

--- split.py
+++ split.py
@@ -4,11 +4,15 @@ import argparse
 parser = argparse.ArgumentParser(description='Basic Arg parser')
 parser.add_argument("--input", default=1, type=str, help="Input Directory",required = True)
 parser.add_argument("--output", default=1, type=str, help="Output Directory",required = True)
-parser.add_argument("--ratio", nargs='+',help="Split ratio, default 0.8,0.1,0.1",required = True)
+parser.add_argument("--ratio", nargs='+',help="Train/val/test or train/val",required = True)
 
 args = parser.parse_args()
 indir = args.input
 outdir = args.output
-ratio = (float(args.ratio[0]),float(args.ratio[1]),float(args.ratio[2]))
+if len(args.ratio) == 3:
+    ratio = (float(args.ratio[0]),float(args.ratio[1]),float(args.ratio[2]))
+else:
+        ratio = (float(args.ratio[0]),float(args.ratio[1]))
+
 
 split_folders.ratio(indir,output = outdir, seed = 1337, ratio = ratio)
